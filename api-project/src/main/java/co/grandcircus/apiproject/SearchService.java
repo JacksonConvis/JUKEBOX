6c0c22d68b55bf718ef389a222f9f2a969858f15
Jack Convis <jackconvis@gmail.com>
2022-07-30 23:30:19 -0400

    changing variable name for java bean's to work

--- api-project/src/main/java/co/grandcircus/apiproject/SearchService.java
+++ api-project/src/main/java/co/grandcircus/apiproject/SearchService.java
@@ -11,7 +11,7 @@ public class SearchService
  {
     private RestTemplate request = new RestTemplate();
 
-    public SearchResponse SearchRes(String searchTerm) {
+    public SearchResponse SearchQuery(String searchTerm) {
         String url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + searchTerm;
         System.out.println(url);
         SearchResponse response = request.getForObject(url, SearchResponse.class);
